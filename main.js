882a0ef4dec31d7919e05905b8cfe655a0e67941
Lanyue <meng890229@gmail.com>
2023-02-03 19:54:21 +0800

    second commit

---
+++ main.js
@@ -0,0 +1,11 @@
+/*window.onload=function(){
+    //document.write("Hello JS!");
+};
+*/
+
+$(document).ready(function(){
+    $("input").click(function(){
+        //alert("Hi");  //test button is alive
+        $("H1").text("Hello"); //putting the text.word on <h1>
+    });
+});
